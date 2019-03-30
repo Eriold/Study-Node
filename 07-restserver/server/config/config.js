69bad542073c6de7d8a528767983b4f2955f758e
Eriold <dacamoty@gmail.com>
2019-03-30 02:02:23 -0500

    Agregar variable de entorno

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://eriold:WrmhUJ1FWlm4WdNA@cluster0-mh50g.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
